9cf9b94e678d3b682db7c14aa48c74fa263bbdb1
Joe Mutuma <joemutuma2001@gmail.com>
2024-10-11 14:59:48 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import About from './sections/About'
+import Clients from './sections/Clients'
+import Contact from './sections/Contact'
+import Experience from './sections/Experience'
+import Footer from './sections/Footer'
+import Hero from './sections/Hero'
+import Navbar from './sections/Navbar'
+import Projects from './sections/Projects'
+
+const App = () => {
+  return (
+    <main>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Projects/>
+      <Clients/>
+      <Experience/>
+      <Contact/>
+      <Footer/>
+     
+    </main>
+    
+  )
+}
+
+export default App
